30fc4fc1ed680f13e65a3fadf9bacf6d71c78235
pbeukema <beuk.pat@gmail.com>
2016-01-09 14:57:46 -0500

    changed line to circle

--- flashlag.py
+++ flashlag.py
@@ -1,33 +1,54 @@
 #!/usr/bin/env python2
 from psychopy import visual, core, event
 import numpy, time
+from math import sin, cos, radians
+from random import shuffle, randint, uniform
+import os, csv
+
+dotSize = .25
+circleRadius = dotSize
 win = visual.Window([800,800], monitor='testMonitor', color=[-1,-1,-1], colorSpace='rgb',
     blendMode='avg', useFBO=True)
 
 handVerts = numpy.array([ [0,0.8],[-0.05,0],[0,-0.05],[5,0] ])#vertices (using numpy means we can scale them easily)
 
-second = visual.ShapeStim(win, vertices= [[0,0.00], [0,0.8]],
-    lineColor=[1,1,1],fillColor=None, lineWidth=6, autoLog=False) #dafuq autoLog?!
+second = visual.ShapeStim(win, vertices= [[0,0.00], [0,0.8]], lineColor=[1,1,1],fillColor=[1,1,1], interpolate=False, lineWidth=1, autoLog=False, autoDraw=False)
 
-flash = visual.ShapeStim(win, vertices= [[0,0.85], [0,1.4]],
-    lineColor=[1,1,1],fillColor=None, lineWidth=6, autoLog=False)
+#flash = visual.ShapeStim(win, vertices= [[0,0.85], [0,1.4]], lineColor=[1,1,1],fillColor=[1,1,1], interpolate=True, lineWidth=1, autoLog=False)
 
-fixation = visual.Circle(win, units = 'pix', radius = 15, lineColor='white', fillColor = 'white')
+fixSpot = visual.GratingStim(win,tex=None, mask="gauss", size=(0.05,0.05),color='white')
 clock = core.Clock() # to grab RTs, -- maybe make global?
 
+
+clockDot = visual.PatchStim(win=win, mask="gauss", size=(0.12,0.12),color='white')
+
+flashDot = visual.PatchStim(win=win, mask="gauss", size=(0.1,0.1),color='yellow')
+
 while True: #ie forever Hard capped to 60Hz refresh unless have better comp but not likely, therefore accuracy limited to +/- min(~16 ms).
-    fixation.draw()
+    fixSpot.draw()
     t = time.localtime()
+    angleRad = radians(second.ori)
+    x = circleRadius*sin(angleRad)
+    y = circleRadius*cos(angleRad)
+    clockDot.setPos([x,y])
+    clockDot.draw()
 
+    flashRadius = circleRadius+.1
+    angleRad = radians(second.ori)
+    x = flashRadius*sin(angleRad)
+    y = flashRadius*cos(angleRad)
+    flashDot.setPos([x,y])
+    #flashDot.draw()
 
-    secPos = 0 #NB floor will round down to previous second
+    #secPos = 0 #NB floor will round down to previous second
 
-    second.ori = second.ori+1   #add adaptive staircase here
-    second.draw()
+    # To make this adaptive just take second.ori and add staircase to adaptRate.
+    second.ori = second.ori+4.5   #add adaptive staircase here
+    #second.draw()
 
-    if second.ori % 90 == 0 and second.ori % 180 != 0 and second.ori % 270 != 0: #jackson pollock looking hack here but its 12:16am and unclear how to do more elegant atm.
-        flash.ori = second.ori
-        flash.draw()
+    if second.ori % 90 == 0: #jackson pollock looking hack here but its 12:16am and unclear how to do more elegant atm.
+        #flash.ori = second.ori
+        flashDot.draw()
 
 
     win.flip()
